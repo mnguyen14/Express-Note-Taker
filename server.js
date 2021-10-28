e166f8388740e16dc4f98ee0ae756aaa9b6ef551
Matthew Nguyen <matthewnguyen0814@gmail.com>
2021-10-28 04:46:10 -0500

    got notes to delete

--- server.js
+++ server.js
@@ -14,26 +14,33 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
-})
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'))
-})
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './db/db.json'))
-})
+    res.sendFile(path.join(__dirname, './db/db.json'));
+});
 
 app.post('/api/notes', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync('./db/db.json'))
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'));
     const newNote = req.body;
-    newNote.id = uuid()
-    notes.push(newNote)
-    fs.writeFileSync('./db/db.json', JSON.stringify(notes, null, 2))
+    newNote.id = uuid();
+    notes.push(newNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes, null, 2));
     res.json(notes);
-})
+});
+
+app.delete(`/api/notes/:id`, (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const deleteNote = notes.filter((remove) => remove.id !== req.params.id);
+    fs.writeFileSync('./db/db.json', JSON.stringify(deleteNote));
+    res.json(deleteNote);
+});
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`)
-})
+});
